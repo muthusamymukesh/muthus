76077155ffa99a3376fa8003309de8c923532f93
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-23 17:00:22 +0530

    Update mmr1.c

--- mmr1.c
+++ mmr1.c
@@ -9,3 +9,21 @@ printf("%calphabet",a);
 else
 printf("%cnot alphabet",a);
 }
+#include<stdio.h>
+int main()
+{
+char a,b,c;
+
+printf("enter");
+scanf("%c %c %c",&a,&b,&c);
+
+if ( a>=b && a<=c )
+printf("%c large number",a);
+
+if ( b>=a && b<=c )
+printf("%c large number",b);
+
+if ( c>=a && c<=b )
+printf("%c large number",c);
+return 0;
+}
